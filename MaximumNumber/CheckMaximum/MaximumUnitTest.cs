3a6fd14517e300f96bd63c2a1ea32306d7bf5563
hem_b <hemanthb93@gmail.com>
2022-05-29 00:06:31 +0530

    [Hemanth]:Check max of 3 float Nos and tested using test cases

--- MaximumNumber/CheckMaximum/MaximumUnitTest.cs
+++ MaximumNumber/CheckMaximum/MaximumUnitTest.cs
@@ -12,7 +12,7 @@ namespace CheckMaximum
 
             int A = 10, B = 9, C = 5;
             MaximumNumber.CheckMaximumOfThree check = new MaximumNumber.CheckMaximumOfThree();
-            var ActualResult = check.CheckMax(A, B, C);
+            var ActualResult = check.CheckIntMax(A, B, C);
             Assert.AreEqual(A, ActualResult);
 
 
@@ -23,7 +23,7 @@ namespace CheckMaximum
 
             int A = 9, B = 10, C = 5;
             MaximumNumber.CheckMaximumOfThree check = new MaximumNumber.CheckMaximumOfThree();
-            var ActualResult = check.CheckMax(A, B, C);
+            var ActualResult = check.CheckIntMax(A, B, C);
             Assert.AreEqual(B, ActualResult);
 
 
@@ -34,9 +34,38 @@ namespace CheckMaximum
 
             int A = 9, B = 5, C = 10;
             MaximumNumber.CheckMaximumOfThree check = new MaximumNumber.CheckMaximumOfThree();
-            var ActualResult = check.CheckMax(A, B, C);
+            var ActualResult = check.CheckIntMax(A, B, C);
             Assert.AreEqual(C, ActualResult);
+        }
+        [Test]
+        public void GivenFloatMaxInFirstPosition_CheckMaxNumber_ReturnMaxInFirstPosition()
+        {
 
+            float A = 10.90f, B = 10.5f, C = 5.0f;
+            MaximumNumber.CheckMaximumOfThree check = new MaximumNumber.CheckMaximumOfThree();
+            var ActualResult = check.CheckFloatMax(A, B, C);
+            Assert.AreEqual(A, ActualResult);
+
+
+        }
+        [Test]
+        public void GivenFloatMaxInSecondPosition_CheckMaxNumber_ReturnMaxInsecondPosition()
+        {
+
+            float A = 10.90f, B = 10.98f, C = 5.0f;
+            MaximumNumber.CheckMaximumOfThree check = new MaximumNumber.CheckMaximumOfThree();
+            var ActualResult = check.CheckFloatMax(A, B, C);
+            Assert.AreEqual(B, ActualResult);
+
+        }
+        [Test]
+        public void GivenFloatMaxInThirsPosition_CheckMaxNumber_ReturnMaxInThirdPosition()
+        {
+
+            float A = 9.0f, B = 5.25f, C = 10.3f;
+            MaximumNumber.CheckMaximumOfThree check = new MaximumNumber.CheckMaximumOfThree();
+            var ActualResult = check.CheckFloatMax(A, B, C);
+            Assert.AreEqual(C, ActualResult);
 
         }
     }
--- MaximumNumber/MaximumNumber/CheckMaximumOfThree.cs
+++ MaximumNumber/MaximumNumber/CheckMaximumOfThree.cs
@@ -8,9 +8,24 @@ namespace MaximumNumber
 {
     public class CheckMaximumOfThree
     {
-        public int CheckMax(int num1,int num2,int num3)
+        public int CheckIntMax(int num1,int num2,int num3)
         {
             if(num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+        public float CheckFloatMax(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
             {
                 return num1;
             }
